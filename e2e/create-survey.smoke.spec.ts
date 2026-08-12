d8c593a25aee0c9655b58bd2d75c409509f5470a
Oboe + Werkstatt <kontakt@oboe-werkstatt.de>
2026-08-12 05:16:02 +0200

    test: cover cancelling survey creation

--- e2e/create-survey.smoke.spec.ts
+++ e2e/create-survey.smoke.spec.ts
@@ -7,3 +7,12 @@ test('opens the new survey page from Home', async ({ page }) => {
   await expect(page).toHaveURL(/\/surveys\/new$/);
   await expect(page.getByRole('heading', { name: 'Create new survey' })).toBeVisible();
 });
+
+test('returns Home when survey creation is cancelled', async ({ page }) => {
+  await page.goto('/surveys/new');
+
+  await page.getByRole('link', { name: 'Cancel' }).click();
+
+  await expect(page).toHaveURL(/\/$/);
+  await expect(page.getByRole('button', { name: 'New survey' })).toBeVisible();
+});
